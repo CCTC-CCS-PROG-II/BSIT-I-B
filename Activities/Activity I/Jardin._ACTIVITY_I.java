a92753a867623741c9677e8e1405d3f871409cd0
jemarjardin23 <157579137+jemarjardin23@users.noreply.github.com>
2024-05-17 17:40:47 +0800

    Update Jardin._ACTIVITY_I.java

---
+++ Activities/Activity I/Jardin._ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class JARDIN_ACTIVITY_I{
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 12;
+        int num3 = 16;
+
+        int sum = num1 + num2 + num3;
+        int diff = num1 - num2 - num3;
+        int prod = num1 * num2 * num3;
+        int quot = num1 / num2 / num3;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(quot);
+
+    }
+}
